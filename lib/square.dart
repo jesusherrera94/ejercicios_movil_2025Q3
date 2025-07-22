2b0197f62594dc13be63e1594d43637da60fd40b
Jesus Herrera <jesusherrera1594@gmail.com>
2025-07-22 17:42:17 -0600

    add lab 1

---
+++ lib/square.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+
+class Square extends StatelessWidget{
+  String message = '';
+  Square({super.key, required this.message});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 100.0,
+      height: 100.0,
+      decoration: BoxDecoration(
+        color: Colors.amber,
+        borderRadius: BorderRadius.circular(8)
+      ),
+      child: Center(
+        child: Text(message, 
+        style: TextStyle( fontWeight: FontWeight.bold,
+         color: Colors.white),),
+      ),
+    );
+  }
+}
