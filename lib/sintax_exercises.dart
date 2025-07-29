4bb68e6c5a32535817129ea9934d5c5a74a44832
Jesus Herrera <jesusherrera1594@gmail.com>
2025-07-28 20:58:47 -0600

    Update basic sintaxis in sintax_exercises.dart

--- lib/sintax_exercises.dart
+++ lib/sintax_exercises.dart
@@ -1,45 +1,61 @@
 
-
 void printHello() {
-  print("Hello world!");
+  print("Hello from printHello()");
 }
 
-void greetingPositional(String name, String? title){
-  if(title != null) {
-    print('Hello! $title $name');
+// funcion con parametros posicionales
+void greetingPositional(String name, String? title) {
+  if (title != null) {
+    print("Hello! $title $name!");
   } else {
-    print('Hello! $name');
+    print("Hello! $name");
   }
 }
 
+// funciones con parametros nombrados
 void greetingNamed({required String name, String? title}){
-  if(title != null) {
-    print('Hello! $title $name');
+  if (title != null) {
+    print("Hello! $title $name!");
   } else {
-    print('Hello! $name');
+    print("Hello! $name");
   }
 }
 
 void main() {
   int num1 = 1;
   double num2 = 1.5;
-  String message = 'Hello world!';
-  print('num1: $num1, num2: $num2, message: $message');
+  String message = "Hello world!";
+  bool isActive = true;
+  print('num1: $num1, num2: $num2, message: $message, isActive: $isActive');
   Map<String, dynamic> myMap = {
     "name": "John",
-    "lastName": "Doe"
+    "lastName": "Doe",
+    "isActive": true,
+    "age": 46,
+    "contacts": ["Juan", "Mario", "Pedro"]
   };
-  print('Last name: ${myMap["lastName"]}, name: ${myMap["name"]}');
-  List<String> games = ['Doom 1993', "Doom II", "Quake", "Doom III"];
-  for(String game in games){
-    print(game);
-  }
-  games.forEach((game) {
-    print(game);
+  print('Map: name: ${myMap["name"]}, lastName: ${myMap["lastName"]}, isActive: ${myMap["isActive"]}, age: ${myMap["age"]}');
+
+  List<String> contacts = myMap["contacts"];
+
+  contacts.forEach((contact){
+    print(contact);
   });
+
+  List<String> games = ["Doom 1993", "Doom II", "Quake", "Doom III"];
+
+  for(String game in games) {
+    print('game: $game');
+  }
+
+  games.forEach((game) {
+    print('Print way 2: $game');
+  });
+
   printHello();
-  greetingPositional('Jesus', null);
-  greetingPositional('Roboto', 'Mr.');
-  greetingNamed(name: 'JesusH');
-  greetingNamed(name: 'Roboto', title: 'Mr.');
+  greetingPositional("Emily", null);
+  greetingPositional("Jack", "Mr.");
+  print("=========================================");
+  greetingNamed(name: "Jack");
+  greetingNamed(title: "Mrs.", name: "Emily");
 }
