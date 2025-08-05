35474b558ebcbfb2532e668a82b29dc7f7950ddd
Jesus Herrera <jesusherrera1594@gmail.com>
2025-08-04 20:54:28 -0600

    Update tab_controller.dart

--- lib/navigation/tab_controller.dart
+++ lib/navigation/tab_controller.dart
@@ -1,30 +1,29 @@
 import 'package:flutter/material.dart';
 import '../screens/home.dart';
-import '../screens/profile.dart';
 import '../screens/store.dart';
+import '../screens/profile.dart';
 
 class TabControllerScreen extends StatelessWidget {
-
-  const TabControllerScreen({super.key});
-
+  
   @override
-  Widget build(BuildContext context) {
-    return DefaultTabController(length: 3, 
-    child: Scaffold(
+  Widget build(Object context) {
+   return DefaultTabController(length: 3, 
+   child: Scaffold(
       appBar: AppBar(
-        title: const Text('My super tab app!'),
-        bottom: const TabBar( tabs: [
-        Tab(icon: Icon(Icons.home),),
-        Tab(icon: Icon(Icons.shopping_cart),),
-        Tab(icon: Icon(Icons.person),),
-      ],),
+        title: const Text("My super app!"),
+        bottom: TabBar(tabs: [
+          Tab(icon: Icon(Icons.home),),
+          Tab(icon: Icon(Icons.shopping_cart),),
+          Tab(icon: Icon(Icons.person),),
+        ]),
       ),
-      body: const TabBarView(children: [
+      // aqui van las pantallas
+      body: TabBarView(children: [
         HomeScreen(),
         StoreScreen(),
         ProfileScreen()
       ]),
-    ));
+   )); 
   }
   
 }
