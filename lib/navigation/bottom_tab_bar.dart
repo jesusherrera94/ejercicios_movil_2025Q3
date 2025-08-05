be287b0dcb8e9d5fa11b03787a7a92e4af36f457
Jesus Herrera <jesusherrera1594@gmail.com>
2025-08-04 20:53:41 -0600

    Update bottom_tab_bar.dart

--- lib/navigation/bottom_tab_bar.dart
+++ lib/navigation/bottom_tab_bar.dart
@@ -1,44 +1,33 @@
 import 'package:flutter/material.dart';
 import '../screens/home.dart';
-import '../screens/profile.dart';
 import '../screens/store.dart';
+import '../screens/profile.dart';
 
 class MyBottomTabBar extends StatefulWidget {
-
   @override
-  State<MyBottomTabBar> createState() => _MyBottomTabBarState(); 
+  State<MyBottomTabBar> createState() => _MyBottomTabBarState();
 }
 
 class _MyBottomTabBarState extends State<MyBottomTabBar> {
-
   int _selectedIndex = 0;
 
   final List<Map<String, dynamic>> _screens = [
-    {
-      "title": 'Home',
-      "screen": HomeScreen()
-    },
-    {
-      "title": 'Store',
-      "screen": StoreScreen()
-    },
-    {
-      "title": 'Profile',
-      "screen": ProfileScreen()
-    }
+    {"title": "Home", "screen": HomeScreen()},
+    {"title": "Store", "screen": StoreScreen()},
+    {"title": "Profile", "screen": ProfileScreen()},
   ];
 
-  void _selectedNewIndex (int newIndex) {
+  void _onTabItem(int newIndex) {
     setState(() {
       _selectedIndex = newIndex;
     });
   }
 
-  Widget? getFoatingActionButton(int index) {
-    if(index == 0) {
-      return FloatingActionButton(onPressed: () {}, child: Icon(Icons.add),);
-    }
-    return null;
+  Widget? getFloatinActionButton(int index) {
+    // si el indice es 0(equivalente a home) retorna el Floating action button, sino retorna nulo
+    return index == 0
+        ? FloatingActionButton(onPressed: () {}, child: Icon(Icons.add))
+        : null;
   }
 
   @override
@@ -46,20 +35,26 @@ class _MyBottomTabBarState extends State<MyBottomTabBar> {
     return Scaffold(
       appBar: AppBar(
         title: Text(_screens[_selectedIndex]["title"]),
-        automaticallyImplyLeading: false, // esto para el futuro!
+        automaticallyImplyLeading: false, // esto nos va a servir en el futuro
       ),
-      body: _screens[_selectedIndex]['screen'],
-      floatingActionButton: getFoatingActionButton(_selectedIndex),
-      bottomNavigationBar: BottomNavigationBar(items: [
-        BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'), // 0
-        BottomNavigationBarItem(icon: Icon(Icons.shopping_cart), label: 'Store'), // 1
-        BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'), // 2
-      ],
-      currentIndex: _selectedIndex,
-      selectedItemColor: Colors.blueAccent,
-      onTap: _selectedNewIndex,
+      body: _screens[_selectedIndex]["screen"],
+      floatingActionButton: getFloatinActionButton(_selectedIndex),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"), // 0
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_cart),
+            label: "Store",
+          ), // 1
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: "Profile",
+          ), // 2
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.blueAccent,
+        onTap: _onTabItem,
       ),
     );
   }
-
 }
