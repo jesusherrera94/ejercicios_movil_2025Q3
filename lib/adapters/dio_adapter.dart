62fc0d701c0c4162bebd3cfafab203f82ac9b301
Jesus Herrera <jesusherrera1594@gmail.com>
2025-08-27 20:51:59 -0600

    Implement postRequest method in DioAdapter
    Added a postRequest method to handle POST requests.

--- lib/adapters/dio_adapter.dart
+++ lib/adapters/dio_adapter.dart
@@ -9,8 +9,14 @@ class DioAdapter {
     _dio = Dio();
   }
 
+
   Future<dynamic> getRequest(String url) async {
     Response<dynamic> response = await _dio.get(url);
+    return response.data;
+  }
+
+  Future<dynamic> postRequest(String url, Map<String, dynamic> body) async {
+    Response<dynamic> response = await _dio.post(url, data: body);
     return response.data;
   }
 
