0aac62ca2e1d7f4ad4b81f1f2e2b3d0dbb38651e
Jesus Herrera <jesusherrera1594@gmail.com>
2025-08-30 09:31:25 -0600

    implement animations

--- lib/navigation/bottom_tab_bar.dart
+++ lib/navigation/bottom_tab_bar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import '../screens/home.dart';
 import '../screens/store.dart';
 import '../screens/profile.dart';
@@ -8,8 +9,13 @@ class MyBottomTabBar extends StatefulWidget {
   State<MyBottomTabBar> createState() => _MyBottomTabBarState();
 }
 
-class _MyBottomTabBarState extends State<MyBottomTabBar> {
+class _MyBottomTabBarState extends State<MyBottomTabBar>
+    with TickerProviderStateMixin {
   int _selectedIndex = 0;
+  late AnimationController _animationController;
+  late Animation<Offset> _offsetAnimation;
+  ScrollController _scrollController = ScrollController();
+  bool _isVisible = true;
 
   final List<Map<String, dynamic>> _screens = [
     {"title": "Home", "screen": HomeScreen()},
@@ -17,24 +23,79 @@ class _MyBottomTabBarState extends State<MyBottomTabBar> {
     {"title": "Profile", "screen": ProfileScreen()},
   ];
 
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 300),
+    );
+    _offsetAnimation = Tween<Offset>(begin: Offset.zero, end: const Offset(0, 1.0))
+        .animate(
+          CurvedAnimation(
+            parent: _animationController,
+            curve: Curves.easeInOut,
+          ),
+        );
+
+    _scrollController.addListener(_scrollListener);
+  }
+
+  void _scrollListener() {
+    if (_scrollController.position.userScrollDirection ==
+        ScrollDirection.reverse) {
+      if (!_isVisible) {
+        _isVisible = true;
+        _animationController.forward();
+      }
+    } else if (_scrollController.position.userScrollDirection ==
+        ScrollDirection.forward) {
+      if (_isVisible) {
+        _isVisible = false;
+        _animationController.reverse();
+      }
+    }
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    _scrollController.dispose();
+    super.dispose();
+  }
+
   void _onTabItem(int newIndex) {
     setState(() {
       _selectedIndex = newIndex;
     });
   }
 
-    Widget? getFloatinActionButton(int index) {
+  Widget? getFloatinActionButton(int index) {
     // si el indice es 0(equivalente a home) retorna el Floating action button, sino retorna nulo
     return index == 0
         ? FloatingActionButton(
-          onPressed: () {
-            Navigator.pushNamed(context, 'create-subscription');
-          },
-          backgroundColor: Colors.blueAccent,
-          shape: CircleBorder(), 
-          child: Icon(Icons.add, color: Colors.white,))
+            onPressed: () {
+              Navigator.pushNamed(context, 'create-subscription');
+            },
+            backgroundColor: Colors.blueAccent,
+            shape: CircleBorder(),
+            child: Icon(Icons.add, color: Colors.white),
+          )
         : null;
   }
+
+  Widget _buildBodyWithScrollController() {
+  switch (_selectedIndex) {
+    case 0:
+      return HomeScreen(scrollController: _scrollController);
+    case 1:
+      return StoreScreen(scrollController: _scrollController);
+    case 2:
+      return ProfileScreen();
+    default:
+      return HomeScreen(scrollController: _scrollController);
+  }
+}
 
   @override
   Widget build(BuildContext context) {
@@ -43,23 +104,26 @@ class _MyBottomTabBarState extends State<MyBottomTabBar> {
         title: Text(_screens[_selectedIndex]["title"]),
         automaticallyImplyLeading: false, // esto nos va a servir en el futuro
       ),
-      body: _screens[_selectedIndex]["screen"],
+      body: _buildBodyWithScrollController(),
       floatingActionButton: getFloatinActionButton(_selectedIndex),
-      bottomNavigationBar: BottomNavigationBar(
-        items: [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"), // 0
-          BottomNavigationBarItem(
-            icon: Icon(Icons.shopping_cart),
-            label: "Store",
-          ), // 1
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            label: "Profile",
-          ), // 2
-        ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: Colors.blueAccent,
-        onTap: _onTabItem,
+      bottomNavigationBar: SlideTransition(
+        position: _offsetAnimation,
+        child: BottomNavigationBar(
+          items: [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"), // 0
+            BottomNavigationBarItem(
+              icon: Icon(Icons.shopping_cart),
+              label: "Store",
+            ), // 1
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              label: "Profile",
+            ), // 2
+          ],
+          currentIndex: _selectedIndex,
+          selectedItemColor: Colors.blueAccent,
+          onTap: _onTabItem,
+        ),
       ),
     );
   }
